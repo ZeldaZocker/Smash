d34e87f84179acb98537579fc8db65eb42c57d4b
ZeldaZocker <34456207+ZeldaZocker@users.noreply.github.com>
2018-01-02 05:53:13 +0100

    v1.4
    Added minimap
    Improved Boundary
    Destroy bullets on ground
    Added Textures

---
+++ Assets/groundDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundDestroy : MonoBehaviour {
+
+public void OnCollisionEnter(Collision other)
+    {
+       if (other.gameObject.tag == "ground")
+        {
+            Debug.Log("Bullet ground!");
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/scripts/destroy_boundary.cs
+++ Assets/scripts/destroy_boundary.cs
@@ -6,12 +6,21 @@ public class destroy_boundary : MonoBehaviour {
 
 	void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag != "bullet")
+        if (other.gameObject.tag == "Player")
         {
-            Debug.Log("Im out!");
             other.transform.position = new Vector3(0, 9, -9);
+            other.GetComponent<jump>().grounded = true;
+            other.GetComponent<jump>().doubleJump = true;
             //Destroy(other.gameObject);
+            Debug.Log("Im back and refreshed!");
         }
+
+        if (other.gameObject.tag == "bullet")
+        {
+            Debug.Log("Bullet out!");
+            Destroy(other.gameObject);
+        }
+
     }
 	
 }
