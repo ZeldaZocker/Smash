6f09bac64315d65592b80b575656788bdc6c8f97
ZeldaZocker <34456207+ZeldaZocker@users.noreply.github.com>
2018-01-20 21:00:19 +0100

    v1.5
    Added Flip
    Added BG Music
    Added shoot sound
    
    Bugged: Bullets doesnt fly straight to the mouse.

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -4,13 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public AudioSource audioSource;
     private Transform myTransform;
     public float speed = 15f;
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
-
-
+    private bool facingRight = true;
+    public float bulletSpeed = 1;
+    public AudioClip shootSound;
     /*  void Start()
       {
           myTransform = GetComponent<Transform>();
@@ -37,9 +38,16 @@ public class PlayerMovement : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
 
-
-
+        var mouse = Input.mousePosition;
+        if (mouse.x - transform.position.x > (Screen.currentResolution.width / 2) && !facingRight)
+            // ... flip the player.
+            Flip();
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (mouse.x - transform.position.x < (Screen.currentResolution.width / 2) && facingRight)
+            // ... flip the player.
+            Flip();
     }
+
 
         void FixedUpdate() {
 
@@ -56,13 +64,23 @@ public class PlayerMovement : MonoBehaviour
         var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
 
         // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.right * 10;
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.right * 10 * bulletSpeed;
+        audioSource.PlayOneShot(shootSound, 1f);
 
         // Destroy the bullet after 2 seconds
         Destroy(bullet, 2.0f);
     }
 
-    
 
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 
 }
--- Assets/scripts/destroy_boundary.cs
+++ Assets/scripts/destroy_boundary.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class destroy_boundary : MonoBehaviour {
+    
+    public int deaths;
 
-	void OnTriggerExit(Collider other)
+
+    void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             other.transform.position = new Vector3(0, 8.8f, -8.6f);
             other.GetComponent<jump>().grounded = true;
             other.GetComponent<jump>().doubleJump = true;
+            deaths++;
             //Destroy(other.gameObject);
             Debug.Log("Im back and refreshed!");
         }
@@ -22,5 +26,10 @@ public class destroy_boundary : MonoBehaviour {
         }
 
     }
-	
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 100, 25), "Deaths: " + deaths.ToString());
+    }
+
 }
